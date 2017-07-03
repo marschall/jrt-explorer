80e1721dbc88994f8f8416aaeab0ed9157e153cb
Philippe Marschall <philippe.marschall@gmail.com>
2017-07-03 15:59:47 +0200

    update to build

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#mvn -o -nsu package && \
-#cp target/jrt-explorer-1.0.0-SNAPSHOT.jar mods/ && \
+mvn -o -nsu package && \
+cp target/jrt-explorer-1.0.0-SNAPSHOT.jar mods/ && \
 java --module-path mods/ -m com.github.marschall.jrtexplorer/com.github.marschall.jrtexplorer.Explorer
-# java -modulepath mods/ -m com.github.marschall.jrtexplorer/com.github.marschall.jrtexplorer.Main
+#java --module-path mods/ -m com.github.marschall.jrtexplorer/com.github.marschall.jrtexplorer.UnsafeExplorer
+#java --module-path mods/ -m com.github.marschall.jrtexplorer/com.github.marschall.jrtexplorer.CompilerExplorer
 
